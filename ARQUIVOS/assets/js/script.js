e8acc443d887001eecec65dc4deebeeb28c23f29
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-06-25 15:28:03 -0300

    Feature(js): Adicionando o Estagio da luta

--- ARQUIVOS/assets/js/script.js
+++ ARQUIVOS/assets/js/script.js
@@ -2,10 +2,10 @@ let char = new Socerer("Samuel");
 
 let monster = new BigMonster();
 
-console.log(`Nome do Monstro: ${monster.name}`);
-console.log(`ataque do Monstro: ${monster.attack}`)
-console.log("_-----------------")
-console.log(char.name);
-console.log(char.life);
-console.log(char.defense);
-console.log(char.attack);
+let stage = new Stage(char,
+    monster,
+    document.querySelector("#char"),
+    document.querySelector("#monster")
+)
+
+stage.start();
